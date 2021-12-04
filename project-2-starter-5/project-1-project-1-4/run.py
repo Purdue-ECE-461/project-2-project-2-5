8bb035be45aa3b640b7ddb84592a508b8c7cd3cb
Rayopter <31804808+Rayopter@users.noreply.github.com>
2021-12-04 12:29:05 -0800

    Delete project-2-starter-5/project-1-project-1-4 directory
    Redundant to new project-1

--- project-2-starter-5/project-1-project-1-4/run.py
+++
@@ -1,6 +0,0 @@
-import sys
-from subprocess import call, run, DEVNULL
-
-output = run(["chmod", "+x", "run_bash_ecngrid.sh"], stdout=DEVNULL, stderr=DEVNULL) #This line is needed for Linux to give permissions
-rc = call(["run_bash_ecngrid.sh",sys.argv[1]])     #uncomment this line for Unix/Linux OS, comment one below
-sys.exit(0)
--- project-2-starter-5/project-1-project-1-4/run_windows.py
+++
@@ -1,5 +0,0 @@
-import sys
-from subprocess import call
-
-rc = call(["run_bash_windows.sh",sys.argv[1]], shell=True) #uncomment this line for Windows OS, comment one above
-sys.exit(0)
